0baa552f99978ff614b9a8c5619e551b06ee5dcd
Chaitanya <35297116+iamchaitu@users.noreply.github.com>
2019-10-25 20:13:21 +0530

    Update fb.py

--- fb.py
+++ fb.py
@@ -4,12 +4,13 @@ import facebook
 import json
 import datetime
 graphAPI = facebook.GraphAPI(access_token)
-profile=graphAPI.get_object("Own-Page")
-result=graphAPI.get_connections(profile['id'],'feed')
-posts=result['data']
-Bday=datetime.datetime.strptime('25-10-2019','%d-%m-%Y')
-for post in posts:
-	pdate=datetime.datetime.strptime(post['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
+#profile=graphAPI.get_object("Own-Page")
+#result=graphAPI.get_connections(profile['id'],'feed')
+#posts=result['data']
+#Bday=datetime.datetime.strptime('25-10-2019','%d-%m-%Y')
+#for post in posts:
+	#pdate=datetime.datetime.strptime(post['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
 	#if (pdate.day == Bday.day and pdate.month == Bday.month):
-	graphAPI.put_post(message)
-	print(post)
+graphAPI.put_post(message)
+graphAPI.put_photo(file_path,caption)
+print(posted Declaration)
