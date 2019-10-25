9118f23f38cb329ead6bd1bf2ecceeb517f74c67
Chaitanya <35297116+iamchaitu@users.noreply.github.com>
2019-10-25 20:02:51 +0530

    Update fb.py

--- fb.py
+++ fb.py
@@ -4,12 +4,12 @@ import facebook
 import json
 import datetime
 graphAPI = facebook.GraphAPI(access_token)
-profile=graphAPI.get_object("me")
+profile=graphAPI.get_object("Own-Page")
 result=graphAPI.get_connections(profile['id'],'feed')
 posts=result['data']
-Bday=datetime.datetime.strptime('31-8-2019','%d-%m-%Y')
+Bday=datetime.datetime.strptime('25-10-2019','%d-%m-%Y')
 for post in posts:
 	pdate=datetime.datetime.strptime(post['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
-	if (pdate.day == Bday.day and pdate.month == Bday.month):
-		graphAPI.put_comment(post['id'],"Thank You")
-		print('posted')
+	#if (pdate.day == Bday.day and pdate.month == Bday.month):
+	#graphAPI.put_like(post['id'],"Thank You")
+	print(post)
